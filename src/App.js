2cabc1eae8b8180cd21f3f3a3c259e945c3d8993
Masse04 <massamba084@gmail.com>
2022-10-13 12:01:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [Recup,setRecup] = useState()
+  const [Task,setTask] = useState([{Task:'la premiere tache', status : false}]);
+  let barre = 'barre'
+  let nonBarre = 'nonBarre'
+ 
+
+  return (
+    <div className="App">
+      <div>
+        <input type = 'text' placeholder = 'ecrire une tâche a effectuer' onChange={(e) => {setRecup(e.target.value)}}/>
+        <button onClick={()=>{setTask([...Task,{Task:Recup , status : false}])
+        console.log(Task)}}>Ajouter</button>
+      </div>
+      <div >
+        {
+          Task.map(el =>(
+              <div className='tache'  >
+                  <input type = 'checkbox'  onChange={()=> {
+                    setTask([...Task.map(num =>
+                      num.Task === el.Task ? {...num, status : !num.status} : num)])    
+                    console.log(Task)
+                  }}/>
+                  <p className={el.status === true ?  barre :  nonBarre}>{el.Task}</p>
+                  <button onClick={()=> {
+                    setTask([...Task.map(x =>
+                      x.Task === el.Task ? Task.pop() : x)])
+                        
+                    console.log(Task)
+                  }}>*</button>
+              </div>
+          ))
+          }
+      </div>
+      
+    </div>
+  );
+}
+export default App;
